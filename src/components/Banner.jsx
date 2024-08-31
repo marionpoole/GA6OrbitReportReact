a4b1cff145567738dbbaa65a97599a4a48fd85e1
Marion Poole <mapoole09@hotmail.com>
2024-08-31 14:46:57 -0500

    Graded Assignment 6 Orbit Report

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import styles from "./styling.css";
+
+function Banner() {
+    return (
+    <div>
+        <header>Orbit Report</header>
+    <p>Instructions for user: Click on the buttons to see the satellites in the selected orbit type</p>
+    </div>
+    )
+}
+
+export default Banner;
